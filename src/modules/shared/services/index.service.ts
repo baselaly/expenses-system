2b5251d550d3a7632a58549d11ac2bed3c02ee62
baselaly <basel.3ly@gmail.com>
2022-11-04 17:52:45 +0200

    NEW : add file upload service

--- src/modules/shared/services/index.service.ts
+++ src/modules/shared/services/index.service.ts
@@ -2,3 +2,4 @@ export { JwtAuthService } from './jwt-auth.service';
 export { DatabaseExceptionService } from './database-exception.service';
 export { PasswordService } from './password.service';
 export { HelperService } from './helper.service';
+export { FileUploadService } from './file-upload.service';
